d9937f6d859e79b5b65d08583b3ae0d67df6dab8
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-10-01 12:21:45 +0600

    Создана модель GuestBook в models.py, модель зарегестрированна в админке, создана миграция, добавлены данные в базу данных

--- guest_book/webapp/admin.py
+++ guest_book/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import GuestBook
 
 # Register your models here.
+
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'email', 'text', 'created_at', 'updated_at', 'status')
+    list_filter = ('id', 'author', 'text', 'created_at', 'updated_at', 'status')
+    search_fields = ('author', 'text', 'created_at', 'updated_at', 'status')
+    fields = ('author', 'email', 'text', 'status')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+
+
+admin.site.register(GuestBook, GuestBookAdmin)
--- guest_book/webapp/models.py
+++ guest_book/webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models import TextChoices
 
-# Create your models here.
+
+
+class StatusChoices(TextChoices):
+    ACTIVE = 'active', 'Активно'
+    BLOCKED = 'blocked', 'Заблокировано'
+
+
+class GuestBook(models.Model):
+    author = models.CharField(max_length=50, null=False, blank=False, verbose_name='Автор')
+    email = models.EmailField(max_length=200, null=False, blank=False, verbose_name='Почта')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст записи')
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    status = models.CharField(max_length=30, null=False, blank=False, choices=StatusChoices.choices, default=StatusChoices.ACTIVE)
+
+
+    def __str__(self) -> str:
+        return f'{self.author} - {self.email} - {self.text}'
